12a50f3d088b88308ee421cc537bf6f8b00c9d73
Duc Xuan Viet Nguyen <dnxv2409@gmail.com>
2026-06-17 13:41:53 +0700

    todo: arithemtics

--- lib/matrix.hpp
+++ lib/matrix.hpp
@@ -1,3 +1,5 @@
+#ifndef MATRIX_H
+#define MATRIX_H
 #include <vector> 
 #include <algorithm>
 #include <cmath>
@@ -7,19 +9,15 @@ class Matrix {
   private: 
   size_t num_rows; 
   size_t num_cols; 
-  std::vector<T> data;
+  std::vector<std::vector<T>> data;
 
   public:
-  size_t numCols() const { return num_cols; }
-  size_t numRows() const { return num_rows; }
-  
-  Matrix() : num_rows(0), num_cols(0) {}
-  
-  Matrix(size_t dimension) : Matrix(dimension, dimension) {}
-  
-  Matrix(size_t rows, size_t cols) : 
-    num_rows(rows), num_cols(cols), data(rows * cols) {}
-  
-  Matrix(size_t rows, size_t cols, const std::vector<T>& v_data) : 
-    num_rows(rows), num_cols(cols), data(v_data) {}
+  size_t numCols() const;
+  size_t numRows() const;
+  Matrix(); 
+  Matrix(size_t dimension); 
+  Matrix(size_t rows, size_t cols); 
+  Matrix(const std::vector<std::vector<T>> & vdata); 
 };
+
+#endif
--- tests/MatrixTests.cpp
+++ tests/MatrixTests.cpp
@@ -1,25 +1,26 @@
 #include <gtest/gtest.h>
 #include "../lib/Matrix.hpp"
+#include "../src/Matrix.cpp"
 
 TEST(MATRIX, INITIALIZE_EMPTY_MATRIX) { 
-  Matrix<float_t> empty_matrix;
-  EXPECT_EQ(0, empty_matrix.numCols());
-  EXPECT_EQ(0, empty_matrix.numRows());
+  Matrix<float_t> m;
+  EXPECT_EQ(static_cast<size_t>(0), m.numCols());
+  EXPECT_EQ(static_cast<size_t>(0), m.numRows());
 }
 
 TEST(MATRIX, INITIALIZE_SQUARE_MATRIX) { 
   std::size_t n = 10; 
-  Matrix<float_t> square_matrix(static_cast<size_t>(n), static_cast<size_t>(n));
-  EXPECT_EQ(n, square_matrix.numCols());
-  EXPECT_EQ(n, square_matrix.numRows());
-  EXPECT_TRUE(square_matrix.numCols() == square_matrix.numRows());
+  Matrix<float_t> m(static_cast<size_t>(n), static_cast<size_t>(n));
+  EXPECT_EQ(n, m.numCols());
+  EXPECT_EQ(n, m.numRows());
+  EXPECT_TRUE(m.numCols() == m.numRows());
 }
 
 TEST(MATRIX, INITIALIZE_NORMAL_MATRIX) { 
   std::size_t rows = 3, cols = 5;
-  Matrix<float_t> matrix(rows, cols);
-  EXPECT_EQ(rows, matrix.numRows());
-  EXPECT_EQ(cols, matrix.numCols());
+  Matrix<float_t> m(rows, cols);
+  EXPECT_EQ(rows, m.numRows());
+  EXPECT_EQ(cols, m.numCols());
 }
 
 int main() {
