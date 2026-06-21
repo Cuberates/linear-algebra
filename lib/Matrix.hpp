ad65b5945636d1a4685bf0f550b1ffa1b7df55da
Duc Xuan Viet Nguyen <dnxv2409@gmail.com>
2026-06-21 16:58:54 +0700

    renamed Matrix.hpp

---
+++ lib/Matrix.hpp
@@ -0,0 +1,27 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+#include <vector> 
+#include <algorithm>
+#include <cmath>
+
+template <typename T> 
+class Matrix { 
+  private: 
+  size_t num_rows; 
+  size_t num_cols; 
+  std::vector<std::vector<T>> data;
+
+  public:
+  [[nodiscard]]
+  size_t numCols() const;
+  [[nodiscard]]
+  size_t numRows() const;
+  Matrix(); 
+  Matrix(size_t dimension); 
+  Matrix(size_t rows, size_t cols); 
+  Matrix(const std::vector<std::vector<T>> & vdata);
+
+  const T& operator() (size_t i, size_t j) const;  
+  T& operator() (size_t i, size_t j); 
+};
+#endif
